4d4de558ffd4ab720aed98ad138c93a17bee0ea1
Veer2008 <75929346+Veer2008@users.noreply.github.com>
2022-03-30 16:56:25 +0530

    Add files via upload

---
+++ config.js
@@ -0,0 +1,9 @@
+export const firebaseConfig = {
+    apiKey: "AIzaSyAet0VZ4XvVkd2vFTfTQ9Ccz506hIHLHIc",
+    authDomain: "storytellingapp-341db.firebaseapp.com",
+    databaseURL: "https://storytellingapp-341db-default-rtdb.firebaseio.com",
+    projectId: "storytellingapp-341db",
+    storageBucket: "storytellingapp-341db.appspot.com",
+    messagingSenderId: "1091871866397",
+    appId: "1:1091871866397:web:8d9cd7067653255ff790f4"
+  };
